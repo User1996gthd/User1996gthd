0d4483408eef1ecbf0ad1f4ef0c1f62ed4a9b045
User1996gthd <89453123+User1996gthd@users.noreply.github.com>
2021-09-28 12:28:45 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,37 @@
 $(document).ready(function(){
-$('.slider').slick();
+$('.slider').slick({
+	 arrows:true,
+	slidesToShow:3,
+responsive: [
+        {breakpoint: 1024, settings: {slidesToShow: 2}},
+        {breakpoint: 500, settings: {slidesToShow: 1}}
+      
+    ]
+	});
+
+
+
+$('.burger-menu').click(function(){
+	$('.burger-menu').toggleClass('active');
+$('.link').toggleClass('mobilemenu');
+
+});
+
+$('.menu__item').click(function(){
+	$('.burger-menu').toggleClass('active');
+$('.link').toggleClass('mobilemenu');
+
+});
+
+$('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+ 
+ 
+        $('body,html').animate({
+            scrollTop: top
+        }, 600);
+  });
 
 });
